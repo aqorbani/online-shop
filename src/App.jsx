b4a15031898ce427ffe66532a22ab3c637b3f772
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-01-16 21:39:23 +0330

    Initial configuration

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center text-center">
+        Online Shop
+      </div>
+    </>
+  );
+}
+
+export default App;
