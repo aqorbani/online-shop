cdc0015b8a693abcad13968b6ad7bdc3961eefc3
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-01-18 11:18:08 +0330

    create loading module

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,5 +1,7 @@
 import ProductsCard from "../components/ProductsCard";
 import { useProducts } from "../context/ProductsContext";
+import Loading from "../components/Loading";
+
 const Products = () => {
   const products = useProducts();
   console.log(products);
@@ -10,7 +12,7 @@ const Products = () => {
       <div className="flex-col md:flex md:flex-row-reverse w-full d-">
         <div className="w-full md:w-1/6">sidebar</div>
         <div className="flex flex-wrap justify-between items-center w-full md:w-5/6">
-          {!products.length && <p>Loading . . .</p>}
+          {!products.length && <Loading wh={80} />}
           {products.map((item) => (
             <ProductsCard key={item.id} data={item} />
           ))}
