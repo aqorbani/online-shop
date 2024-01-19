026954063045b51a98521c38738fbb8f6d8e8f13
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-01-19 16:42:09 +0330

    Create Cart and Product Details page

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,22 +1,39 @@
 import { Link } from "react-router-dom";
 import { HiMiniShoppingCart } from "react-icons/hi2";
 import PropTypes from "prop-types";
+import { FaReact } from "react-icons/fa6";
+import { SiTailwindcss } from "react-icons/si";
+import { useCart } from "../context/CartContext";
 
 const Layout = ({ children }) => {
   Layout.propTypes = {
     children: PropTypes.object,
   };
 
+  const [state] = useCart();
+
   return (
     <>
       <header className="flex justify-between p-2 m-2 bg-white border border-gray-200 rounded-lg shadow">
-        <Link className="sign">Online Shop</Link>
-        <Link className="btn">
-          <HiMiniShoppingCart />
+        <Link to="/products" className="sign">
+          <div className="flex">
+            <div className="flex p-2 pt-3">
+              <FaReact />
+              +
+              <SiTailwindcss />
+            </div>
+            <p className="p-2">Online Shop</p>
+          </div>
+        </Link>
+        <Link to="/checkout" className="inline-flex items-center rounded-md bg-lime-900 px-2 py-1 text-xs font-bold text-white">
+          {!!state.itemsCounter && (
+            <span className="p-1">{state.itemsCounter}</span>
+          )}
+          <HiMiniShoppingCart className="p-1 text-2xl" />
         </Link>
       </header>
       {children}
-      <footer></footer>
+      <footer className="flex justify-center p-2 m-2 bg-white border border-gray-200 rounded-lg shadow">© 2024 A.Qorbani . All rights reserved.</footer>
     </>
   );
 };
